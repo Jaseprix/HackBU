36e125a81d3c20b8bcaa3b7f444a6de91448b248
sunkistcap <75499948+sunkistcap@users.noreply.github.com>
2023-02-05 11:54:21 -0500

    Create locations.py

---
+++ locations.py
@@ -0,0 +1,81 @@
+restaurant = [
+    "Five Guys",
+    "Olive Garden",
+    "Red Lobster",
+    "Nirchi's Pizza",
+    "Chipotle",
+    "Social on State",
+    "Little Venice Restaurant",
+    "Lost Dog Café & Lounge",
+    "Pho Nhu Y Restaurant",
+    "Hacienda Mexican Restaurant",
+    "Texas Roadhouse",
+    "P S Restaurant",
+    "The Shop",
+    "Tully's"]
+  
+bar = [
+    "Abel's Pub",
+    "The Ale House",
+    "Coppertop Tavern",
+    "Irish Kevins",
+    "Sach's Tee House",
+    "Johnnie's Tavern",
+    "Sliderz Bar & Grill",
+    "Beer Tree"]
+  
+mall = [
+    "Binghamton Plaza",
+    "Vestal Plaza",
+    "Riverside Plaza",
+    "Oakdale Mall",
+    "Town Square",
+    "Northgate Plaza",
+    "Campus Plaza",
+    "Parkway Plaza",
+    "Westover Plaza"]
+  
+Museum = [
+    "BU Art Museum",
+    "Bundy Museum of History and Art",
+    "Roberson Museum and Science Center",
+    "Endicott History and Heritage Center",
+    "Vestal Museum",
+    "Phelps Mansion Museum",
+    "TechWorks!",
+    "Broome County Historical Society",
+    "Rockbottom Dam Firefighter's Memorial",
+    "Susquehanna County Historical Society"]
+  
+movies = [
+    "Horror/Thriller",
+    "Comedy",
+    "Romance",
+    "Action",
+    "Documentary",
+    "Indie",
+    "Sci-fi",
+    "Drama",
+    "Western",
+    "Animation",
+    "Superhero"]
+  
+outdoors = [
+    "BU Nature Preserve",
+    "Lake Liberman",
+    "Finch Hollow Nautre Center",
+    "King Nautre Preserve",
+    "Woodbourne Forest And Wildlife Preserve",
+    "Wildwood Nature Resreve",
+    "Oakley Corners State Forest",
+    "Logan Hill Nature Preserve",
+    "The Rail Trail",
+    "Owego Riverwalk",
+    "Vestal Rail Trail",
+    "Finger Lakes Trail",
+    "Chenango River Walk",
+    "Wittman Dam",
+    "abbott Loop",
+    "Aqua-Terra Wilderness Area",
+    "Eldridge Wilderness Trail",
+    "Park"]
